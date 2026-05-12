225cd9e5df798650e26b59cd3ff7cb345740515e
Husna Bayraktar <husnabayraktar@Husnas-Air.lan>
2026-05-12 13:52:08 -0400

    Add AWS credentials for Streamlit Cloud

--- dashboard.py
+++ dashboard.py
@@ -2,9 +2,15 @@ import streamlit as st
 import boto3
 import json
 import pandas as pd
+import os
 
 BUCKET_NAME = "job-postings-raw-husna"
-s3 = boto3.client('s3', region_name='us-east-1')
+s3 = boto3.client(
+    's3',
+    region_name=os.environ.get('AWS_DEFAULT_REGION', 'us-east-1'),
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+)
 
 def load_jobs():
     jobs = []
